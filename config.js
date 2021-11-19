305602d27f329a44322d171f62261bebac0a1f59
mujin2021 <94691514+mujin2021@users.noreply.github.com>
2021-11-19 21:05:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        bannar_coming: "点击",
+        balloons_flying: "好像少点东西哟",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
